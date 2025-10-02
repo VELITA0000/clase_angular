6b41e08be5b3689a9531a0b43c60ee3e1e294771
vela <dvela020@gmail.com>
2025-10-02 13:30:33 -0600

    input, enter y funciones para el usuario

--- src/app/pages/movies/movies.ts
+++ src/app/pages/movies/movies.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MovieItem } from './movie-item/movie-item';
 import { Movie as IMovie } from '../../shared/types/movie';   // interfaz
 import { Movie } from '../../shared/services/movie';          // servicio
 
 @Component({
   selector: 'app-movies',
-  imports: [CommonModule, MovieItem],
+  imports: [CommonModule, MovieItem, FormsModule],
   templateUrl: './movies.html',
   styleUrl: './movies.scss'
 })
 
 export class Movies implements OnInit {
   movies: IMovie[] = [];
+  
+  title = ''; // propiedad asociada a un binder de propiedad en el html
+
+  confirmed = true;
   
   constructor(private movieService: Movie) {}
 
@@ -26,6 +31,13 @@ export class Movies implements OnInit {
   }
   
   handleMovieSelected(movie: IMovie) {
-    this.movieService.setMovie(movie);
+    this.movieService.setMovie(movie)
   }
+
+  addMovie() {
+    alert('Agregar: ' + this.title);
+    this.title = '';
+  }
+
+
 }
